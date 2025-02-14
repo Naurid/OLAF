08f41947d31e0e78e22a132d43f0135970996248
Gabor Vilics <vilics@hotmail.com>
2025-02-14 12:29:15 +0100

    Program.cs changé

--- DisneyBattle.WebAPI/Program.cs
+++ DisneyBattle.WebAPI/Program.cs
@@ -1,10 +1,17 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Data.Common;
+using DisneyBattle.WebAPI.Repos;
+using DisneyBattle.WebAPI.Services;
+using Microsoft.Data.SqlClient;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<DbConnection>(
+    s => new SqlConnection(builder.Configuration.GetConnectionString("Dev")));
+builder.Services.AddTransient<ILieuRepository, LieuService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,30 +22,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
-
+app.MapControllers();
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
