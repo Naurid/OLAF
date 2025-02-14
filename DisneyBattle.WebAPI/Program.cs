1d15cdbba984ce09ae6bb79ca89e19664bbc45ab
Epestia <dylan.menegon@hotmail.com>
2025-02-14 13:59:29 +0100

    controller api insert Update  GetById  GetAll de personnage

--- DisneyBattle.WebAPI/Program.cs
+++ DisneyBattle.WebAPI/Program.cs
@@ -1,13 +1,27 @@
+
+using DisneyBattle.Services;
+using DisneyBattle.Interfaces;
+using DisneyBattle.WebAPI.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Ajout des services
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Configuration de la chaîne de connexion
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Injection des dépendances
+builder.Services.AddTransient<IPersonnageRepository, PersonnageRepository>(provider =>
+    new PersonnageRepository(connectionString));
+
+builder.Services.AddTransient<PersonnageService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configuration du pipeline HTTP
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -15,30 +29,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
-
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
---
+++ DisneyBattle.WebAPI/Repos/PersonnageRepository.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.Data.SqlClient;
+using DisneyBattle.WebAPI.Models;
+using System.Collections.Generic;
+using DisneyBattle.WebAPI.Models.Stock.Models;
+
+
+namespace DisneyBattle.WebAPI.Repositories
+{
+    public class PersonnageRepository : IPersonnageRepository
+    {
+        private readonly string _connectionString;
+
+        public PersonnageRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public bool Insert(PersonnageModel personnage)
+        {
+            string query = "INSERT INTO personnages (nom, alignement_id, niveau, experience, points_vie, points_attaque, points_defense, lieu_id) VALUES (@Nom, @AlignementId, @Niveau, @Experience, @PointsVie, @PointsAttaque, @PointsDefense, @LieuId)";
+
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Nom", personnage.Nom);
+                cmd.Parameters.AddWithValue("@AlignementId", personnage.AlignementId);
+                cmd.Parameters.AddWithValue("@Niveau", personnage.Niveau);
+                cmd.Parameters.AddWithValue("@Experience", personnage.Experience);
+                cmd.Parameters.AddWithValue("@PointsVie", personnage.PointsVie);
+                cmd.Parameters.AddWithValue("@PointsAttaque", personnage.PointsAttaque);
+                cmd.Parameters.AddWithValue("@PointsDefense", personnage.PointsDefense);
+                cmd.Parameters.AddWithValue("@LieuId", personnage.LieuId);
+
+                conn.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                return rowsAffected > 0;
+            }
+        }
+
+        public bool Update(int id, PersonnageModel personnage)
+        {
+            string query = "UPDATE personnages SET nom = @Nom, alignement_id = @AlignementId, niveau = @Niveau, experience = @Experience, points_vie = @PointsVie, points_attaque = @PointsAttaque, points_defense = @PointsDefense, lieu_id = @LieuId WHERE id = @Id";
+
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Id", id);
+                cmd.Parameters.AddWithValue("@Nom", personnage.Nom);
+                cmd.Parameters.AddWithValue("@AlignementId", personnage.AlignementId);
+                cmd.Parameters.AddWithValue("@Niveau", personnage.Niveau);
+                cmd.Parameters.AddWithValue("@Experience", personnage.Experience);
+                cmd.Parameters.AddWithValue("@PointsVie", personnage.PointsVie);
+                cmd.Parameters.AddWithValue("@PointsAttaque", personnage.PointsAttaque);
+                cmd.Parameters.AddWithValue("@PointsDefense", personnage.PointsDefense);
+                cmd.Parameters.AddWithValue("@LieuId", personnage.LieuId);
+
+                conn.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                return rowsAffected > 0;
+            }
+        }
+
+        public PersonnageModel GetById(int id)
+        {
+            PersonnageModel personnage = null;
+            string query = "SELECT id, nom, alignement_id, niveau, experience, points_vie, points_attaque, points_defense, lieu_id FROM personnages WHERE id = @id";
+
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+                conn.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        personnage = new PersonnageModel
+                        {
+                            Id = reader.GetInt32(0),
+                            Nom = reader.GetString(1),
+                            AlignementId = reader.GetInt32(2),
+                            Niveau = reader.GetInt32(3),
+                            Experience = reader.GetInt32(4),
+                            PointsVie = reader.GetInt32(5),
+                            PointsAttaque = reader.GetInt32(6),
+                            PointsDefense = reader.GetInt32(7),
+                            LieuId = reader.GetInt32(8)
+                        };
+                    }
+                }
+            }
+            return personnage;
+        }
+
+        public IEnumerable<PersonnageModel> GetAll()
+        {
+            var personnages = new List<PersonnageModel>();
+            string query = "SELECT id, nom, alignement_id, niveau, experience, points_vie, points_attaque, points_defense, lieu_id FROM personnages";
+
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        personnages.Add(new PersonnageModel
+                        {
+                            Id = reader.GetInt32(0),
+                            Nom = reader.GetString(1),
+                            AlignementId = reader.GetInt32(2),
+                            Niveau = reader.GetInt32(3),
+                            Experience = reader.GetInt32(4),
+                            PointsVie = reader.GetInt32(5),
+                            PointsAttaque = reader.GetInt32(6),
+                            PointsDefense = reader.GetInt32(7),
+                            LieuId = reader.GetInt32(8)
+                        });
+                    }
+                }
+            }
+            return personnages;
+        }
+    }
+}
